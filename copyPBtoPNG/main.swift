da2a17e3dcede9273c02ccbd2d23595fdbee0e7b
Kai <kai.koehn@gmail.com>
2015-11-28 15:24:14 +0100

    first commit

--- copyPBtoPNG/main.swift
+++ copyPBtoPNG/main.swift
@@ -1,12 +1,25 @@
-//
-//  main.swift
-//  copyPBtoPNG
-//
-//  Created by Kautz on 28.11.15.
-//  Copyright © 2015 Kautz. All rights reserved.
-//
+//: Playground - noun: a place where people can play
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
+extension NSImage {
+    var imagePNGRepresentation: NSData {
+        return NSBitmapImageRep(data: TIFFRepresentation!)!.representationUsingType(.NSPNGFileType, properties: [:])!
+    }
+    func savePNG(path:String) -> Bool {
+        return imagePNGRepresentation.writeToFile(path, atomically: true)
+    }
+}
 
+let pb = NSPasteboard.generalPasteboard()
+let typeArr:[AnyClass] = [NSImage.self]
+
+let tmp = pb.readObjectsForClasses(typeArr, options: nil)
+if tmp?.count > 0 {
+    let img = tmp![0] as! NSImage
+    
+    let good_b64 = img.imagePNGRepresentation.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+    let finalResult = "data:image/png;base64," + good_b64;
+    pb.clearContents()
+    pb.writeObjects([finalResult])
+}
